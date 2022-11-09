92dfa20a3b817925bb25f549b82ab5c2da69a5d1
ElskaWitch <justinelabas95@gmail.com>
2022-11-09 11:35:29 +0100

    responsive immobilier ok

--- resources/views/pages/immobilier.blade.php
+++ resources/views/pages/immobilier.blade.php
@@ -4,8 +4,8 @@
 						@include('components.sections.Section-houses')
 				</div>
 		</div>
-		<div class="pt-24 px-24">
-				<div class="bg-black text-white font-semibold text-center text-xl rounded-full whitespace-pre-line py-5 ">
+		<div class="pt-24 px-3 md:px-24">
+				<div class="bg-black text-white font-semibold text-center md:text-xl rounded-xl md:rounded-full whitespace-pre-line py-5 px-3 md:px-0 ">
 						<p>La proximité est un élément de base 2M Immobilier. <br>
 								L'immobilier, c'est avant tout de l'humain. Nous nous attachons à comprendre chaque personne et chaque projet de
 								vie. <br>
