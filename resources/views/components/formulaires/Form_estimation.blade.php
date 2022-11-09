86d344762ee9c350f1c0f9b4e44345373e2c8222
ElskaWitch <justinelabas95@gmail.com>
2022-11-09 11:32:23 +0100

    responsive estimation ok

--- resources/views/components/formulaires/Form_estimation.blade.php
+++ resources/views/components/formulaires/Form_estimation.blade.php
@@ -7,58 +7,62 @@
 		<form action="" class="text-center" method="POST">
 				@csrf
 				{{-- type --}}
-				<div class="">
+				<div class="px-3 md:px-0">
 						<input class="input w-full max-w-xs" name="type" placeholder="Type de bien*" type="text" />
 						<x-error-msg name="type" />
 				</div>
 				{{-- adresse/ville --}}
-				<div class="text-center text-2xl font-bold py-10">
-						<h2>
-								Je renseigne <span class="uppercase">l'adresse</span>
-						</h2>
-				</div>
-				<div class="flex justify-center space-x-10">
-						{{-- adresse --}}
-						<div class="">
-								<input class="input w-full max-w-xs" placeholder="Adresse" type="text" />
+				<div class="px-3 md:px-0">
+						<div class="text-center text-2xl font-bold py-10">
+								<h2>
+										Je renseigne <span class="uppercase">l'adresse</span>
+								</h2>
 						</div>
-						{{-- ville --}}
-						<div class="">
-								<input class="input w-full max-w-xs" placeholder="Ville" type="text" />
+						<div class="flex justify-center space-x-10">
+								{{-- adresse --}}
+								<div class="">
+										<input class="input w-full max-w-xs" placeholder="Adresse" type="text" />
+								</div>
+								{{-- ville --}}
+								<div class="">
+										<input class="input w-full max-w-xs" placeholder="Ville" type="text" />
+								</div>
 						</div>
 				</div>
 				{{-- coordoné --}}
-				<div class="text-center text-2xl font-bold py-10">
-						<h2>
-								Je renseigne <span class="uppercase">mes coordonnées</span>
-						</h2>
-				</div>
-				<div class="flex justify-center space-x-10 pb-10">
-						{{-- nom --}}
-						<div class="">
-								<input class="input w-full max-w-xs" name="nom" placeholder="Nom*" type="text" />
-								<x-error-msg name="nom" />
+				<div class="px-3 md:px-0">
+						<div class="text-center text-2xl font-bold py-10">
+								<h2>
+										Je renseigne <span class="uppercase">mes coordonnées</span>
+								</h2>
 						</div>
-						{{-- prenom --}}
-						<div class="">
-								<input class="input w-full max-w-xs" name="prenom" placeholder="Prenom*" type="text" />
-								<x-error-msg name="prenom" />
+						<div class="flex justify-center space-x-10 pb-10">
+								{{-- nom --}}
+								<div class="">
+										<input class="input w-full max-w-xs" name="nom" placeholder="Nom*" type="text" />
+										<x-error-msg name="nom" />
+								</div>
+								{{-- prenom --}}
+								<div class="">
+										<input class="input w-full max-w-xs" name="prenom" placeholder="Prenom*" type="text" />
+										<x-error-msg name="prenom" />
+								</div>
 						</div>
-				</div>
-				<div class="flex justify-center space-x-10 pb-10">
-						{{-- email --}}
-						<div class="">
-								<input class="input w-full max-w-xs" name="email" placeholder="E-mail*" type="text" />
-								<x-error-msg name="email" />
-						</div>
-						{{-- tel --}}
-						<div class="">
-								<input class="input w-full max-w-xs" name="tel" placeholder="Téléphone*" type="text" />
-								<x-error-msg name="tel" />
+						<div class="flex justify-center space-x-10 pb-10">
+								{{-- email --}}
+								<div class="">
+										<input class="input w-full max-w-xs" name="email" placeholder="E-mail*" type="text" />
+										<x-error-msg name="email" />
+								</div>
+								{{-- tel --}}
+								<div class="">
+										<input class="input w-full max-w-xs" name="tel" placeholder="Téléphone*" type="text" />
+										<x-error-msg name="tel" />
+								</div>
 						</div>
 				</div>
 				{{-- condition --}}
-				<div class="py-5 flex justify-center space-x-3">
+				<div class="py-5 flex justify-center space-x-3 px-3 md:px-0">
 						<input checked="checked" class="checkbox" type="checkbox" />
 						<p>J’accepte les conditions d’utilisation des données
 								(*)* <br>
@@ -70,7 +74,7 @@
 						<button class="bg-white text-xl font-semibold px-3 py-1 rounded-xl text-black">Envoyer</button>
 				</div>
 		</form>
-		<div class="px-32 py-10">
+		<div class="px-3 md:px-32 py-10">
 				<p>
 						Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par La Boite Immo
 						agissant comme Sous-traitant du traitement pour la gestion de la clientèle/prospects de l'Agence / du Réseau qui
--- resources/views/pages/estimation.blade.php
+++ resources/views/pages/estimation.blade.php
@@ -1,10 +1,10 @@
 <x-main-layout title="Estimation">
 	{{-- section estimation --}}
-		<div class="py-24 px-32">
+		<div class="py-24 px-3 md:px-32">
 				@include('components.sections.Section-estimation')
 		</div>
 		{{-- formulaire --}}
-		<div class="px-32">
+		<div class="px-3 md:px-32">
 				@include('components.formulaires.Form_estimation')
 		</div>
 </x-main-layout>
